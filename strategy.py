4dc1ca825b582653ca40676090a0b7a3c05915f3
lwamuhaji <lwamuhaji@gmail.com>
2021-11-06 16:27:02 +0900

    initial commit

--- strategy.py
+++ strategy.py
@@ -1,147 +1,149 @@
-from DerivativesTrader import DerivativesTrader
-import time
+from derivativesTrader import DerivativesTrader
+import Responses
+from typing import NamedTuple
 
-class Strategy:
+class Account(NamedTuple):
+    buyOrder: Responses.OrderWithStatus = None
+    sellOrder: Responses.OrderWithStatus = None
 
-    def __init__(self) -> None:
-        self.__createTrader()
-        self.__orderCount = 0
-        self.__openTH = self.trader.openTH
-        self.__closeTH = self.trader.closeTH
+class Params(NamedTuple):
+    openTH: float
+    closeTH: float
+    qty: float
+    orderCount: int
+    qtyWeight: float
+    THWeight: float
 
-    def __createTrader(self) -> None:
-        self.trader = DerivativesTrader(api_key="MdojZrGYvNXsXf8sxp", 
-                                        api_secret="jLNEDFu2CGJip7h8UlheZVDX1yMI20V8L6LW", 
-                                        symbol='EOSUSDT', 
-                                        openTH=0.01, 
-                                        closeTH=0.0003, 
-                                        interval='1', 
-                                        limit=99, 
-                                        openWeight=1.5, 
-                                        closeWeight=1.5)
+class Spread(NamedTuple):
+    open_buyPrice: float = 0
+    open_sellPrice: float = 0
+    close_buyPrice: float = 0
+    close_sellPrice: float = 0
 
-    def checkBeforeStart(self) -> None:
-        for position in self.trader.getPositions().result:
-            if position['size'] > 0:
-                raise Exception('You already have positions')
+class DerivativesStrategy:
 
-    def __initValues(self):
-        self.qty = self.trader.getQtyStep()
-        self.price = self.trader.getTickSize()
+    def __init__(self, api_key, api_secret, symbol, openTH, closeTH, qtyWeight, THWeight) -> None:
+        self.trader: DerivativesTrader = DerivativesStrategy.__createTrader(api_key, api_secret, symbol)
+        self.params = Params(openTH=openTH, closeTH=closeTH, 
+                             qty=self.trader.symbol.QTYSTEP, 
+                             qtyWeight=qtyWeight, THWeight=THWeight, 
+                             orderCount=0)
+        self.account = Account()
+        self.spread = Spread()
 
-    def start(self):
-        self.checkBeforeStart()
-        self.__initValues()
-        print('Start trading')
-        self.__start()
+    def __createTrader(api_key, api_secret, symbol) -> DerivativesTrader:
+        return DerivativesTrader(api_key, api_secret, symbol)
 
-    def onOrderExecuted(self, side):
-        self.updateTH()
-        if side=='Buy':
-            self.onLongExecuted()
-        elif side=='Sell':
-            self.onShortExecuted()
+    def updateSpread(self):
+        average_price = self.trader.getAvgPrice()
+
+        upper_spread = (lambda avg, threshold, count: avg * (1 + threshold) ** count)
+        lower_spread = (lambda avg, threshold, count: avg * (1 - threshold) ** count)
+
+        self.spread.open_buyPrice = upper_spread(average_price, self.params.openTH, self.params.orderCount)
+        self.spread.open_sellPrice = lower_spread(average_price, self.params.openTH, self.params.orderCount)
+
+        self.spread.close_buyPrice = lower_spread(average_price, self.params.openTH, self.params.orderCount)
+        self.spread.close_sellPrice = upper_spread(average_price, self.params.openTH, self.params.orderCount)
+
+    def buy(self):
+        self.updateSpread()
+        self.account.buyOrder = self.trader.placeOrder(side='Buy', qty=self.params.qty, price=self.spread.open_buyPrice)
+
+    def sell(self):
+        self.updateSpread()
+        self.account.sellOrder = self.trader.placeOrder(side='Sell', qty=self.params.qty, price=self.spread.open_sellPrice)
+
+    def close_buy(self):
+        self.updateSpread()
+        self.trader.setTradingStop(side='Buy', price=self.spread.close_buyPrice)
+
+    def close_sell(self):
+        self.updateSpread()
+        self.trader.setTradingStop(side='Sell', price=self.spread.close_sellPrice)
     
-    def updateTH(self):
-        self.trader.openTH *= self.trader._openWeight
-        if self.__orderCount != 0:
-            self.trader.closeTH *= self.trader._closeWeight
-        self.__orderCount += 1
+    def cancelBuy(self):
+        self.trader.cancelOrder(self.account.buyOrder)
 
-    def onLongExecuted(self):
-        self.longOrder = self.placeOrder('Buy')
-        if self.shortOrder != None:
-            self.trader.cancelOrder(self.shortOrder.order_id)
-        self.shortOrder = None
+    def cancelSell(self):
+        self.trader.cancelOrder(self.account.sellOrder)
 
-    def onShortExecuted(self):
-        self.shortOrder = self.placeOrder('Sell')
-        if self.longOrder != None:
-            self.trader.cancelOrder(self.longOrder.order_id)
-        self.longOrder = None
+    def onBuyOrderFilled(self):
+        self.params.orderCount += 1
+        self.close_buy()
+        self.cancelSell()
+        self.buy()
 
-    def updateQty(self):
-        pass
+        while True:
+            if self.buyOrderFilled():
+                self.onBuyOrderFilled()
+                break
+            elif self.trader.hasPosition == False:
+                print('Closed')
+                break
+            else:
+                self.refreshBuyOrder()
+                self.close_buy()
+        print('Trading Completed')
 
-    def updateOpenPrice(self):
-        return self.trader.getOpenSpread()
+    def onSellOrderFilled(self):
+        self.params.orderCount += 1
+        self.close_sell()
+        self.cancelBuy()
+        self.sell()
 
-    def placeOrder(self, side):
-        top, bottom = self.updateOpenPrice()
-        top, bottom = round(top,3), round(bottom, 3)
-        price = bottom if side=='Buy' else top
-        return self.trader.placeOrder(side=side, qty=self.qty, price=price)
+        while True:
+            if self.sellOrderFilled():
+                self.onSellOrderFilled()
+                break
+            elif self.trader.hasPosition == False:
+                print('Closed')
+                break
+            else:
+                self.refreshSellOrder()
+                self.close_sell()
+        print('Trading Completed')
 
-    def replaceOrder(self, side):
-        top, bottom = self.updateOpenPrice()
-        top, bottom = round(top,3), round(bottom, 3)
-        price = bottom if side=='Buy' else top
-        order = self.longOrder if side=='Buy' else self.shortOrder
-        return self.trader.replaceOrder(order_id=order.order_id, price=price)
-
-    def isOrderExcuted(self):
-        L, S = None, None
-        if self.longOrder != None:
-            L = self.trader.queryOrder(self.longOrder).order_status
-        if self.shortOrder != None:
-            S = self.trader.queryOrder(self.shortOrder).order_status
-        if L == 'Filled':
-            return 'Buy'
-        if S == 'Filled':
-            return 'Sell'
-        return False
-
-    def hasPosition(self):
-        positions = self.trader.getPositions()
-        if positions.longPosition['size'] > 0:
-            return 'Buy'
-        elif positions.shortPosition['size'] > 0:
-            return 'Sell'
+    def buyOrderFilled(self):
+        if self.trader.queryOrder(self.account.buyOrder).order_status == 'Filled':
+            return True
         else:
             return False
 
-    def updateClosePrice(self):
-        return self.trader.getCloseSpread()
+    def sellOrderFilled(self):
+        if self.trader.queryOrder(self.account.sellOrder).order_status == 'Filled':
+            return True
+        else:
+            return False
 
-    def closePosition(self, side):
-        top, bottom = self.updateClosePrice()
-        top, bottom = round(top,3), round(bottom, 3)
-        price = bottom if side=='Buy' else top
-        self.trader.setTradingStop(side, price)
+    def refreshBuyOrder(self):
+        pass
 
-    def resetAll(self):
-        self.__orderCount = 0
-        self.trader.openTH = self.__openTH
-        self.trader.closeTH = self.__closeTH
+    def refreshSellOrder(self):
+        pass
 
-    def updateOrders(self):
-        L, S = None, None
-        if self.longOrder != None:
-            L = self.trader.queryOrder(self.longOrder).order_status
-        if self.shortOrder != None:
-            S = self.trader.queryOrder(self.shortOrder).order_status
-        if L == 'New':
-            self.replaceOrder('Buy')
-        if S == 'New':
-            self.replaceOrder('Sell')
-            
+class MyStrategy(DerivativesStrategy):
+    
+    def __init__(self, api_key, api_secret, symbol, openTH, closeTH, qtyWeight, THWeight) -> None:
+        super().__init__(api_key, api_secret, symbol, openTH, closeTH, qtyWeight, THWeight)
 
-    def __start(self):
-        self.longOrder = self.placeOrder(side='Buy')
-        self.shortOrder = self.placeOrder(side='Sell')
+    def startTrade(self) -> int:
+        if self.trader.hasPosition():
+            return -1
+        
+        self.buy()
+        self.sell()
 
+        #어느 한 쪽의 주문이 체결될 때까지 대기
         while True:
-            time.sleep(0.5)
-            order_flag = self.isOrderExcuted()
-            if order_flag:
-                self.onOrderExecuted(order_flag)
+            if self.buyOrderFilled():
+                self.onBuyOrderFilled()
+                break
 
-            time.sleep(0.5)
-            position_flag = self.hasPosition()
-            if position_flag:
-                self.closePosition(position_flag)
-            elif position_flag == False and self.__orderCount > 0:
-                raise Exception('end')
+            elif self.sellOrderFilled():
+                self.onSellOrderFilled()
+                break
 
-            time.sleep(0.5)
-            self.updateOrders()
+            else:
+                self.refreshBuyOrder()
+                self.refreshSellOrder()
